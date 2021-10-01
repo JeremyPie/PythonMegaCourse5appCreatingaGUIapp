0c42786db7d48d6a11826f1e0e9eff4ff952c622
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-10-02 00:13:52 +0300

    Create script2.py

---
+++ tests/script2.py
@@ -0,0 +1,38 @@
+import psycopg2
+
+def create_table():
+    cur.execute("CREATE TABLE IF NOT EXISTS store (item TEXT, quantity INEGER, price FLOAT)")
+
+def insert(item, quantity, price):
+    conn =  psycopg2.connect("dbname='database1' user='postgres' password=postgres123' hpst='localhost' port='5432'")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES (%,%,%)", (item, quantity, price))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn =  psycopg2.connect("dbname='database1' user='postgres' password=postgres123' hpst='localhost' port='5432'")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM store")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(item):
+    conn =  psycopg2.connect("dbname='database1' user='postgres' password=postgres123' hpst='localhost' port='5432'")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item = %", (item,))
+    conn.commit()
+    conn.close()
+
+def update(quantity, price, item):
+    conn = psycopg2.connect("dbname='database1' user='postgres' password=postgres123' hpst='localhost' port='5432'")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity=%, price=% WHERE item=%", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+create_table()    
+#update(11, 6, "Water Glass")
+#delete("Wine Glass")
+#print(view())
