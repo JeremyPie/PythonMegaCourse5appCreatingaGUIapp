495f22381fe12464cf356779fbb55d4089e58869
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-10-01 11:40:37 +0300

    .

--- tests/kg_to_gm_lb_oz.py
+++ tests/kg_to_gm_lb_oz.py
@@ -3,8 +3,8 @@ from tkinter import *
 window = Tk()
 
 def kg_to_gm_lb_oz():
-    e1_value = float(e1_value.get())
-    gm, lb, oz = e1_value * 1000, e1_value * 2.20462, e1_value * 35.274
+    kg = float(e1_value.get())
+    gm, lb, oz = kg * 1000, kg * 2.20462, kg * 35.274
     t1.delete('1.0', END)
     t2.delete('1.0', END)
     t3.delete('1.0', END)
@@ -15,21 +15,21 @@ def kg_to_gm_lb_oz():
 
 l1 =Label(window, text='Kg')
 l1.grid(row=0, column=0)
-   
+
 e1_value = StringVar()
 e1 = Entry(window, textvariable=e1_value)
 e1.grid(row=0, column=1)
 
-b1 = Button(window.text = 'Convert', command=kg_to_gm_lb_oz)
+b1 = Button(window, text = 'Convert', command=kg_to_gm_lb_oz)
 b1.grid(row=0, column=2)
 
 t1 = Text(window, height=1, width=20)
-t1.grid(row=0, column=0)
+t1.grid(row=1, column=0)
 
 t2 = Text(window, height=1, width=20)
-t2.grid(row=0, column=2)
+t2.grid(row=1, column=1)
 
 t3 = Text(window, height=1, width=20)
-t3.grid(row=0, column=2)
+t3.grid(row=1, column=2)
 
 window.mainloop()
--- tests/km_to_miles.py
+++ tests/km_to_miles.py
@@ -5,7 +5,7 @@ window = Tk()
 def km_to_miles():
     miles = float(e1_value.get()) * 1.6
     t1.insert(END, miles)
-b1 = Button(window.text = 'Execute', command=km_to_miles)
+b1 = Button(window, text = 'Execute', command=km_to_miles)
 b1.grid(row=0, column=0)
 
 e1_value = StringVar()
