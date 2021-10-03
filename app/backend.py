099e394ab5c31ff1036f0948715115a19c9640e0
JeremyPie <91102181+JeremyPie@users.noreply.github.com>
2021-10-03 11:30:40 +0300

    .

--- app/backend.py
+++ app/backend.py
@@ -1,33 +1,33 @@
 import sqlite3
 
 class Database():
-    def __init__(self):
-        self.conn = sqlite3.connect("books.db")
-        self.cur = self.conn.cur()
-        self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMERY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
+    def __init__(self, database):
+        self.conn = sqlite3.connect("database")
+        self.cur = self.conn.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title TEXT, author TEXT, year INTEGER, isbn INTEGER)")
         self.conn.commit()
 
-    def insert(self):
-        self.cur.execute("INSERT INTO book VALUES (?,?,?,?)", (title, author, year, isbn)
-        self.conn.commit()                   
-    
+    def insert(self, title, author, year, isbn):
+        self.cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)", (title, author, year, isbn))
+        self.conn.commit()
+
     def view(self):
         self.cur.execute("SELECT * FROM book")
         rows = self.cur.fetchall()
         return rows
-                
+
     def search(self, title="", author="", year="", isbn=""):
-        self.cur.execute("SELECT * FROM bool WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
+        self.cur.execute("SELECT * FROM book WHERE title=? OR author=? OR year=? OR isbn=?", (title, author, year, isbn))
         rows = self.cur.fetchall()
         return rows
-                
+
     def delete(self, id):
         self.cur.execute("DELETE FROM book WHERE id=?", (id,))
         self.conn.commit()
-                
+
     def update(self, id, title, author, year, isbn):
-        self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (itle, author, year, isbn, id))
+        self.cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title, author, year, isbn, id))
         self.conn.commit()
-    
+
     def __del__(self):
         self.conn.close()
--- app/gui.py
+++ app/gui.py
@@ -1,24 +1,24 @@
 from tkinter import *
 from backend import Database
 
-database = Database("book.db")       
+database = Database("book.db")
 
 class Window():
-    def __init__(self,window):  
+    def __init__(self,window):
         self.window = window
         self.window.wm_title("Bookstore")
-        
+
         l1 = Label(self.window, text = "Title")
         l1.grid(row=0, column=0)
 
         l2 = Label(self.window, text = "Author")
-        l1.grid(row=0, column=2)
+        l2.grid(row=0, column=2)
 
         l3 = Label(self.window, text = "Year")
-        l1.grid(row=1, column=0)
+        l3.grid(row=1, column=0)
 
         l4 = Label(self.window, text = "ISBN")
-        l1.grid(row=1, column=2)
+        l4.grid(row=1, column=2)
 
         self.title_text = StringVar()
         self.e1 = Entry(self.window, textvariable = self.title_text)
@@ -36,70 +36,72 @@ class Window():
         self.e4 = Entry(self.window, textvariable = self.isbn_text)
         self.e4.grid(row=1, column=3)
 
-        self.list1 = Listbox(self.window, height=6, width=35, rowspan=6, columnspan=2)
-        self.list1.grid(row=2, column=0)
-        self.list1.bind('<<ListboxSelect>>' get_selected_row)
+        self.list1 = Listbox(self.window, height=6, width=35)
+        self.list1.grid(row=2, column=0, rowspan=6, columnspan=2)
+        self.list1.bind('<<ListboxSelect>>', self.get_selected_row)
 
-        sb1 = Scrolbar(self.window)
+        sb1 = Scrollbar(self.window)
         sb1.grid(row=2, column=2, rowspan=6)
-        self.list1.configure(yscorllcommand = sb1.set)
+        self.list1.configure(yscrollcommand = sb1.set)
         sb1.configure(command = self.list1.yview)
 
-        b1 = Button(self.window, text = "View all", command = view_command)
+        b1 = Button(self.window, text = "View all", command = self.view_command)
         b1.grid(row=2, column=3)
 
-        b1 = Button(self.window, text = "Search entry", command = search_command)
-        b1.grid(row=2, column=3)
+        b2 = Button(self.window, text = "Search entry", command = self.search_command)
+        b2.grid(row=3, column=3)
 
-        b1 = Button(self.window, text = "Add entry", command = add_command)
-        b1.grid(row=3, column=3)
- 
-        b1 = Button(self.window, text = "Update", command = update_command)
-        b1.grid(row=4, column=3)
- 
-        b1 = Button(self.window, text = "Delete", command = delete_command)
-        b1.grid(row=5, column=3)
+        b3 = Button(self.window, text = "Add entry", command = self.add_command)
+        b3.grid(row=4, column=3)
 
-        b1 = Button(self.window, text = "Close", command = self.window.destroy)
-        b1.grid(row=6, column=3) 
-                              
-    def view_command():
-        self.list1.delete(0, END)
-        for row in backend.view():
-            self.list1.insert(END, row)
-        
-    def search_command():
-        self.list1.delete(0, END)
-        for row in backend.search(self.title_text.get(), self.author_text.get(), int(self.year_text.get()), int(self.isbn_text.get())):
-            self.list1.insert(END, row)
-        
-    def add_command():
-        backend.insert(self.title_text.get(), self.author_text.get(), int(self.year_text.get()), int(self.isbn_text.get()))
-        self.list1.delete(0, END)
-        self.list1.insert(END, self.title_text.get(), self.author_text.get(), int(self.year_text.get()), int(self.isbn_text.get()))
+        b4 = Button(self.window, text = "Update", command = self.update_command)
+        b4.grid(row=5, column=3)
 
-    def get_selected_row(event):
+        b5 = Button(self.window, text = "Delete", command = self.delete_command)
+        b5.grid(row=6, column=3)
+
+        b6 = Button(self.window, text = "Close", command = self.window.destroy)
+        b6.grid(row=7, column=3)
+
+
+    def get_selected_row(self, event):
+        print(f"{self.list1.curselection()[0]}")
+        print(f"{self.list1.get(self.list1.curselection()[0])[0]}")
         try:
-            global selected_id
-            selected_id.curselecion()[0]
+            self.selected_id = self.list1.curselection()[0]
             self.e1.delete(0, END)
             self.e2.delete(0, END)
             self.e3.delete(0, END)
             self.e4.delete(0, END)
-            self.e1.insert(END, selected_tuple[1])
-            self.e2.insert(END, selected_tuple[2])
-            self.e3.insert(END, selected_tuple[3])
-            self.e4.insert(END, selected_tuple[4])
-            return selected_tuple
+            self.e1.insert(END, self.list1.get(self.selected_id)[1])
+            self.e2.insert(END, self.list1.get(self.selected_id)[2])
+            self.e3.insert(END, self.list1.get(self.selected_id)[3])
+            self.e4.insert(END, self.list1.get(self.selected_id)[4])
+            return self.selected_id
         except:
-            pass
+            print("skipped")
 
-    def delete_command():
-        backend.delete(selected_id)
-    
-    def update_command():
-        backend.update(selected_id, self.title_text.get(), self.author_text.get(), int(self.year_text.get()), int(self.isbn_text.get())
-                   
+    def view_command(self):
+        self.list1.delete(0, END)
+        for row in database.view():
+            self.list1.insert(END, row)
+
+    def search_command(self):
+        self.list1.delete(0, END)
+        for row in database.search(self.title_text.get(), self.author_text.get(), int(self.year_text.get()), int(self.isbn_text.get())):
+            self.list1.insert(END, row)
+
+    def add_command(self):
+        database.insert(self.title_text.get(), self.author_text.get(), int(self.year_text.get()), int(self.isbn_text.get()))
+        self.list1.delete(0, END)
+        self.list1.insert(END, (self.title_text.get(), self.author_text.get(), int(self.year_text.get()), int(self.isbn_text.get())))
+
+    def delete_command(self):
+        database.delete(self.selected_id + 1)
+
+    def update_command(self):
+        database.update(self.selected_id + 1, self.title_text.get(), self.author_text.get(), int(self.year_text.get()), int(self.isbn_text.get()))
+
 window = Tk()
 Window(window)
 window.mainloop()
